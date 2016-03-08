1b8febbb2ead6c44997ed3d6397c0df703fb6c05
HEBRARD GEOFFROY p1511544 <geoffroyhebrard@laposte.net>
2016-03-08 15:56:14 +0100

    modification db

--- db/content.sql
+++ db/content.sql
@@ -1,6 +1,6 @@
 insert into VideoGames values
-( 'Pacman', 'Jeu interessant','Jeu tres interessant','Mr.Pacman',2013,'http://www4.pcmag.com/media/images/345821-the-history-of-pac-man.jpg?thumb=y','sport',153,3);
+(1, 'Pacman', 'Jeu interessant','Jeu tres interessant','Mr.Pacman',2013,'http://www4.pcmag.com/media/images/345821-the-history-of-pac-man.jpg?thumb=y','sport',153,3);
 
 
 insert into Users values
-('bob@hotmail.fr','bobixdu01','Dupont','Bob','3 rue pierrot',14785,'Besoul');
+(1,'bob@hotmail.fr','L2nNR5hIcinaJkKR+j4baYaZjcHS0c3WX2gjYF6Tmgl1Bs+C9Qbr+69X8eQwXDvw0vp73PrcSeT0bGEW5+T2hA==','Dupont','Bob','3 rue pierrot',14785,'Besoul','YcM=A$nsYzkyeDVjEUa7W9K','ROLE_USER');
