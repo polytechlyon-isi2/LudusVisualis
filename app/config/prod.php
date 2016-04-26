203e72c54d6f3921ba8abf0c03203833d7f158de
Quentin <quentin.tournier2@etu.univ-lyon1.fr>
2016-04-26 19:38:56 +0200

    déploiement openshift

--- app/config/prod.php
+++ app/config/prod.php
@@ -4,9 +4,8 @@
 $app['db.options'] = array(
     'driver'   => 'pdo_mysql',
     'charset'  => 'utf8',
-    'host'     => 'localhost',
-    'port'     => '3306',
-    'dbname'   => 'ludusvisualis',
-    'user'     => 'Ludus_user',
-    'password' => 'secret',
+    'host'     => getenv('OPENSHIFT_MYSQL_DB_HOST'),
+    'dbname'   => getenv('OPENSHIFT_GEAR_NAME'),
+    'user'     => getenv('OPENSHIFT_MYSQL_DB_USERNAME'),
+    'password' => getenv('OPENSHIFT_MYSQL_DB_PASSWORD'),
 );
--- includes/footer.php
+++
@@ -1,8 +0,0 @@
-<footer>
-    <?php
-$pdo = null;
-?>
-        <p2> Ce site a été réalisé par Tournier Quentin et Geoffroy Hebrard</p2>
-        <script src="../web/lib/jquery/jquery.min.js"></script>
-        <script src="../web/lib/bootstrap/js/bootstrap.min.js"></script>
-</footer>
