f266133b2e5e0d13b20be667aa0de2b08d01314e
Quentin <quentin.tournier2@etu.univ-lyon1.fr>
2016-06-03 17:01:02 +0200

    dernier changement qui marche vraiment

--- app/config/prod.php
+++ app/config/prod.php
@@ -4,16 +4,13 @@
 $app['db.options'] = array(
     'driver'   => 'pdo_mysql',
     'charset'  => 'utf8',
-    'host'     => 'localhost',
-    'dbname'   => 'ludusvisualis',
-    'user'     => 'Ludus_user',
-    'password' => 'secret',
+     'host'     => getenv('OPENSHIFT_MYSQL_DB_HOST'),
+    'dbname'   => getenv('OPENSHIFT_GEAR_NAME'),
+    'user'     => getenv('OPENSHIFT_MYSQL_DB_USERNAME'),
+    'password' => getenv('OPENSHIFT_MYSQL_DB_PASSWORD'),
       
 );
 
  /*
 
-    'host'     => getenv('OPENSHIFT_MYSQL_DB_HOST'),
-    'dbname'   => getenv('OPENSHIFT_GEAR_NAME'),
-    'user'     => getenv('OPENSHIFT_MYSQL_DB_USERNAME'),
-    'password' => getenv('OPENSHIFT_MYSQL_DB_PASSWORD'),*/
+   */
